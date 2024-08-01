575dc274862c30583bfef3c9ca19e86285cea69d
Indy Sierra <isierra2694@gmail.com>
2024-07-31 18:01:07 -0700

    sync

--- src/Scene.js
+++ src/Scene.js
@@ -8,35 +8,35 @@ import { Model } from './Model';
 import { Earth } from './Earth';
 
 const Scene = () => {
-	const scroll = useScroll();
-	const { width, height } = useThree((state) => state.viewport);
-	const model = useRef(null);
-	const camera = useRef(null);
+    const scroll = useScroll();
+    const { width, height } = useThree((state) => state.viewport);
+    const model = useRef(null);
+    const camera = useRef(null);
+    const skybox = useRef(null);
 
-	useFrame((state, delta) => {
-		camera.current.position.z = -scroll.scroll.current * 10;
-		model.current.position.z = -scroll.scroll.current * 10 - 10;
-	});
+    useFrame(() => {
+        const scrollProgress = scroll.scroll.current;
+    });
 
-	return (
-		<>
-			<group ref={camera}>
-				<PerspectiveCamera />
-			</group>
-			<Model ref={model} position={[0, -2, 0]} rotation={[0, Math.PI / 2, 0.25]} />		
-			<Earth position={[20, -15, -10]} rotation={[1, 2, 0]}/>
-			<Stars radius={50} depth={100} count={3000} factor={5} saturation={0} fade speed={1} />
-			<ambientLight color="white" intensity={0.1} />
-			<directionalLight color="white" intensity={1.5} position={[0, 100, 100]} />
-			<mesh scale={500}>
-				<sphereGeometry args={[1, 100, 100]} />
-				<LayerMaterial side={THREE.BackSide}>
-					<Noise colorA="#0a0b47" colorB="#0b0c26" colorC="#071442" colorD="#0e0f47" alpha={1} scale={0.7} offset={[0, 0, 0]} />
-					<Depth colorA="black" colorB="black" alpha={0.2} mode="normal" near={0} far={300} origin={[100, 100, 100]} />
-				</LayerMaterial>
-			</mesh>
-		</>
-	);
-}
+    return (
+        <>
+            <group ref={camera}>
+                <PerspectiveCamera makeDefault fov={60} position={[-20, 0, 0]} rotation={[0, -Math.PI / 2, 0]}/>
+            </group>
+            <Model ref={model} />
+            <Earth position={[50, 50, -200]} rotation={[1, 2, 0]} />
+            <Stars radius={50} depth={100} count={3000} factor={5} saturation={0} fade speed={1} />
+            <ambientLight color="white" intensity={0.1} />
+            <directionalLight color="white" intensity={1.5} position={[0, 100, 100]} />
+            <mesh scale={500} ref={skybox}>
+                <sphereGeometry args={[1, 100, 100]} />
+                <LayerMaterial side={THREE.BackSide}>
+                    <Noise colorA="#0a0b47" colorB="#0b0c26" colorC="#071442" colorD="#0e0f47" alpha={1} scale={0.7} offset={[0, 0, 0]} />
+                    <Depth colorA="black" colorB="black" alpha={0.2} mode="normal" near={0} far={300} origin={[100, 100, 100]} />
+                </LayerMaterial>
+            </mesh>
+        </>
+    );
+};
 
 export default Scene;
